5ee2034ac48f24c2634dac7beefce497c0267e86
Salman S. Rahman <salmansr1995@gmail.com>
2021-11-15 17:59:36 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,21 @@
+// ===== number =======
+
+var num1 = 20;
+var num2 = 30;
+
+var num = num1 + num2;
+console.log(num)
+
+// ====== string ====== 
+
+var name1 = "Taskin"
+var name2 = " Asghar"
+var name = name1 + name2;
+console.log(name)
+
+
+// ======== boolean ========
+
+var isExpensive = true;
+
+console.log(isExpensive)
